be9e000b8e659f41a0f89540689bee9b9a9a90e2
Ronan Muller <ronanmuller1980@gmail.com>
2025-12-16 17:10:42 -0300

    inclusao dos logs no datadog

--- src/NotificacaoPubSub.Api/Program.cs
+++ src/NotificacaoPubSub.Api/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Compact;
 using System.IO;
 
 
@@ -10,12 +13,37 @@ namespace NotificacaoPubSub.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("service", "notificacao-pubsub-api")
+                .Enrich.WithProperty("env", "local")
+                .WriteTo.File(
+                    new CompactJsonFormatter(),
+                    @"C:\logs\notificacao-api\log-.json",
+                    rollingInterval: RollingInterval.Day
+                )
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
+
         public static IWebHostBuilder CreateHostBuilder(string[] args) =>
-           WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseContentRoot(Directory.GetCurrentDirectory());
+             WebHost.CreateDefaultBuilder(args)
+                 .UseStartup<Startup>()
+                 .ConfigureLogging(logging =>
+                 {
+                     logging.ClearProviders(); // remove console, debug etc
+                     logging.AddSerilog();     // conecta ILogger ao Serilog
+                 })
+                 .UseContentRoot(Directory.GetCurrentDirectory());
     }
 }
--- src/NotificacaoPubSub.Service/Extensions/ServiceColletionExtensions.cs
+++ src/NotificacaoPubSub.Service/Extensions/ServiceColletionExtensions.cs
@@ -6,6 +6,12 @@ using NotificacaoPubSub.Domain.Interfaces.Services;
 using NotificacaoPubSub.Domain.Models.Configuracao;
 using NotificacaoPubSub.Domain.Profiles;
 using NotificacaoPubSub.Service.Services;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace NotificacaoPubSub.Service.Extensions
 {
@@ -30,9 +36,73 @@ namespace NotificacaoPubSub.Service.Extensions
             services.AddAutoMapper(typeof(AssinaturaTopicoProfile).Assembly);
             services.AddAutoMapper(typeof(TopicoProfile).Assembly);
 
+            services.ConfigureOpenTelemetry();
             services.AddHealthChecks();
 
             return services;
+        }
+
+        [ExcludeFromCodeCoverage]
+        public static void ConfigureOpenTelemetry(this IServiceCollection services)
+        {
+            const string serviceName = "notificacaopubsub";
+            const string activitySourceName = "notificacaopubsub";
+
+            // Endpoint OTLP (Datadog / OpenTelemetry Collector)
+            var otlpEndpoint = new Uri("http://localhost:4318/v1/traces");
+
+            services.AddOpenTelemetry()
+                .ConfigureResource(resource =>
+                    resource.AddService(serviceName))
+
+                // Tracing
+                .WithTracing(tracing =>
+                {
+                    tracing
+                        .AddSource(activitySourceName)                        // Spans customizados
+                        .AddAspNetCoreInstrumentation(options =>              // Instrumentação automática ASP.NET Core
+                        {
+                            options.RecordException = true;
+
+                            // Tags customizadas na requisição
+                            options.EnrichWithHttpRequest = (activity, request) =>
+                            {
+                                activity.SetTag("app.environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+                                activity.SetTag("http.method", request.Method);
+                                activity.SetTag("http.url", request.Path);
+                            };
+
+                            // Tags customizadas na resposta
+                            options.EnrichWithHttpResponse = (activity, response) =>
+                            {
+                                activity.SetTag("http.status_code", response.StatusCode);
+                            };
+
+                            // Captura exceptions
+                            options.EnrichWithException = (activity, exception) =>
+                            {
+                                activity.SetTag("error", true);
+                                activity.SetTag("error.message", exception.Message);
+                            };
+                        })
+                        .AddHttpClientInstrumentation()                        // Instrumentação automática HttpClient
+                        .AddOtlpExporter(options =>                             // Exporta traces para Datadog via OTLP
+                        {
+                            options.Endpoint = otlpEndpoint;
+                        });
+                })
+
+                // Logging
+                .WithLogging(logging =>
+                {
+                    logging.AddOtlpExporter(options =>                       // Exporta logs via OTLP
+                    {
+                        options.Endpoint = otlpEndpoint;
+                    });
+                });
+
+            // ActivitySource para spans customizados
+            services.AddSingleton(new ActivitySource(activitySourceName));
         }
     }
 }
